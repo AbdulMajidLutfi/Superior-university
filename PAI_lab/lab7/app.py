bfc7f07848e2486dcf049cbb66560a5d28a0c6be
AbdulMajidLutfi <lutfimajid456@gmail.com>
2025-05-02 08:06:48 -0700

    lab7 add

---
+++ PAI_lab/lab7/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+from weather import OpenWeatherAPI
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    weather = None
+    error = None
+    location = ""
+    if request.method == "POST":
+        location = request.form["location"]
+        weather_api = OpenWeatherAPI(api_key="8f85c24bd5674261b50236721d2842c9")
+        weather, error = weather_api.fetch_weather(location)
+        if weather:
+            weather.save_to_csv(location)
+    return render_template("index.html", weather=weather, error=error, location=location)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
---
+++ PAI_lab/lab7/weather.py
@@ -0,0 +1,38 @@
+import csv
+import requests
+class Weather:
+    def __init__(self, temperature, humidity, pressure, wind_speed):
+        self._temperature = temperature
+        self._humidity = humidity
+        self._pressure = pressure
+        self._wind_speed = wind_speed
+
+    def save_to_csv(self, location):
+        with open('weather_data.csv', mode='a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([location,
+                             self._temperature,
+                             self._humidity,
+                             self._pressure,
+                             self._wind_speed])
+
+class WeatherAPI:
+    def __init__(self, api_key):
+        self._api_key = api_key
+    def fetch_weather(self, location):
+        raise NotImplementedError()
+class CurrentWeather(Weather):
+    pass
+class OpenWeatherAPI(WeatherAPI):
+    def fetch_weather(self, location):
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={self._api_key}&units=metric"
+        response = requests.get(url)
+        data = response.json()
+        if response.status_code != 200 or 'main' not in data or 'wind' not in data:
+            return None, data.get('message', 'Unknown error')
+        weather = CurrentWeather(
+            data["main"]["temp"],
+            data["main"]["humidity"],
+            data["main"]["pressure"],
+            data["wind"]["speed"])
+        return weather, None
